fb375629ff57c01e6e580ca2d914b1121566e40f
Gaultier Delbarre <gaultiertexas@gmail.com>
2020-03-19 11:51:23 -0500

    Implemented simple stone drawing on board. Need to make automatic

--- BoardDraw.py
+++ BoardDraw.py
@@ -19,7 +19,7 @@ class stoneMove:
     owner: chr
     side: int = -1
 
-moveList = [stoneMove(2, '1', 0)] 
+moveList = [stoneMove(2, '1', 0), stoneMove(3, '2', 1), stoneMove(6, '1')] 
 
 def drawBoard(stdscr):
     curses.noecho()
@@ -77,7 +77,19 @@ def drawBoard(stdscr):
                             stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), 'X')
                     if (x == (7*xscale) and 2*yscale < y < 4*yscale) or (y == 3*yscale and 6*xscale < x < 8*xscale):
                         stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), 'X')
-
+                    
+                    for stone in moveList:
+                        tmp = []
+                        if stone.pos in [0,1,2,3,12,13,14] and stone.side == -1:
+                            continue
+                        if stone.pos not in [0,1,2,3,12,13,14] and stone.side != -1:
+                            stone.side = -1
+                        if stone.side != -1:
+                            tmp = place_table[stone.pos][stone.side]
+                        else:
+                            tmp = place_table[stone.pos]
+                        if x == tmp[0]*xscale and y == tmp[1]*yscale:
+                            stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), '#')
 
                     if x % (2*xscale) == 0:
                         stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_VLINE)
