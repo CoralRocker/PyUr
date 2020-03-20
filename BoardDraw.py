21fed6d4d07406d1137678459fd903f10440fefe
Gaultier Delbarre <gaultiertexas@gmail.com>
2020-03-19 19:17:55 -0500

    Refined board drawing

--- BoardDraw.py
+++ BoardDraw.py
@@ -18,15 +18,22 @@ class stoneMove:
     pos: int
     owner: chr
     side: int = -1
-
-moveList = [stoneMove(2, '1', 0), stoneMove(3, '2', 1), stoneMove(6, '1')] 
+    pid: int = 1
 
 def drawBoard(stdscr):
     curses.noecho()
     curses.curs_set(0)
     stdscr.keypad(True)
     stdscr.clear()
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_color(11, 1000, 0, 0)
+    curses.init_color(12, 0, 1000, 0)
+    curses.init_pair(1, 11, -1)
+    curses.init_pair(2, 12, -1)
 
+    moveList = [stoneMove(2, '1', 0, 2), stoneMove(3, '2', 1, 1), stoneMove(13, curses.ACS_CKBOARD, pid=1, side=0)] 
+    
     dbf = open("debug", "w")
 
     height, width = stdscr.getmaxyx()
@@ -43,41 +50,43 @@ def drawBoard(stdscr):
         yscale = int(xscale/2)
     dbf.write(f"POST XSCL {xscale} YSCL {yscale}\n")
 
+    cellSize = (2*xscale - 1, 2*yscale - 1)
+
     while True:
         stdscr.erase()
         for y in range(0, (6*yscale) + 1):
             for x in range(0, (16*xscale) + 1):
                 if y == 0:
                     if x in [0, (12*xscale)]:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_ULCORNER)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_ULCORNER)
                     elif x in [(8*xscale), (16*xscale)]:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_URCORNER)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_URCORNER)
                     elif x < (12*xscale) and x > (8*xscale):
                         continue
                     elif x % (2*xscale) == 0:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_TTEE)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_TTEE)
                     else:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_HLINE)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_HLINE)
                 elif y == (6*yscale):
                     if x in [0, (12*xscale)]:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_LLCORNER)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_LLCORNER)
                     elif x in [(8*xscale), (16*xscale)]:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_LRCORNER)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_LRCORNER)
                     elif x < (12*xscale) and x > (8*xscale):
                         continue
                     elif x % (2*xscale) == 0:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_BTEE)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_BTEE)
                     else:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_HLINE)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_HLINE)
                 elif y % (2*yscale) != 0:
                     if 0 < y < (2*yscale) or  (4*yscale) < y < (6*yscale):
                         if x < (12*xscale) and x > (8*xscale):
                             continue
                         if x in [xscale, 13*xscale] or (y in [yscale, 5*yscale] and (0 < x < 2*xscale or 12*xscale < x < 14*xscale)):
-                            stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), 'X')
+                            stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), 'X')
                     if (x == (7*xscale) and 2*yscale < y < 4*yscale) or (y == 3*yscale and 6*xscale < x < 8*xscale):
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), 'X')
-                    
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), 'X')
+                    '''
                     for stone in moveList:
                         tmp = []
                         if stone.pos in [0,1,2,3,12,13,14] and stone.side == -1:
@@ -89,24 +98,55 @@ def drawBoard(stdscr):
                         else:
                             tmp = place_table[stone.pos]
                         if x == tmp[0]*xscale and y == tmp[1]*yscale:
-                            stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), '#')
-
+                            if cellSize[0] <= 3 or cellSize[1] <= 3:
+                                stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), stone.owner)
+                            else:
+                                smaller = cellSize[0] if cellSize[0] <= cellSize[1] else cellSize[1]
+                                for y2 in range(y-int((smaller-1)/2-1), y+int((smaller-1)/2)):
+                                    for x2 in range(x-int((smaller-1)/2-1), x+int((smaller-1)/2)):
+                                        stdscr.addch(int((height/2 - (6*yscale)/2) + y2), int((width/2 - (16*xscale)/2) + x2), stone.owner)
+                    '''
                     if x % (2*xscale) == 0:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_VLINE)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_VLINE)
                 elif y in [2*yscale,4*yscale]:
                     if x == 0:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_LTEE)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_LTEE)
                     elif x == (16*xscale):
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_RTEE)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_RTEE)
                     elif x == (10*xscale):
                         if y == (2*yscale):
-                            stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_TTEE)
+                            stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_TTEE)
                         elif y == (4*yscale):
-                            stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_BTEE)
+                            stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_BTEE)
                     elif x % (2*xscale) == 0:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_PLUS)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_PLUS)
                     else:
-                        stdscr.insch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_HLINE)
+                        stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), curses.ACS_HLINE)
+
+        for y in range(0, 6*yscale, yscale):
+            for x in range(0, 16*xscale, xscale):
+                #if y in [yscale, 3*yscale, 5*yscale]:
+                for stone in moveList:
+                    tmp = []
+                    if stone.pos in [0,1,2,3,12,13,14] and stone.side == -1:
+                        continue
+                    if stone.pos not in [0,1,2,3,12,13,14] and stone.side != -1:
+                        stone.side = -1
+                    if stone.side != -1:
+                        tmp = place_table[stone.pos][stone.side]
+                    else:
+                        tmp = place_table[stone.pos]
+                    if x == tmp[0]*xscale and y == tmp[1]*yscale:
+                        if cellSize[0] <= 3 or cellSize[1] <= 3:
+                            stdscr.addch(int((height/2 - (6*yscale)/2) + y), int((width/2 - (16*xscale)/2) + x), stone.owner)
+                        else:
+                            smaller = cellSize[0] if cellSize[0] <= cellSize[1] else cellSize[1]
+                            for y2 in range(y-int((smaller-1)/2-1), y+int((smaller-1)/2)):
+                                for x2 in range(x-int((smaller-1)/2-1), x+int((smaller-1)/2)):
+                                    if x == x2 and y == y2:
+                                        stdscr.addch(int((height/2 - (6*yscale)/2) + y2), int((width/2 - (16*xscale)/2) + x2), curses.ACS_DIAMOND)
+                                    else:
+                                        stdscr.addch(int((height/2 - (6*yscale)/2) + y2), int((width/2 - (16*xscale)/2) + x2), stone.owner, curses.color_pair(stone.pid))
 
 
         stdscr.refresh()
@@ -127,6 +167,6 @@ def drawBoard(stdscr):
                 xscale -= 1 if xscale % 2 == 1 else 0
                 yscale = int(xscale/2)
             dbf.write(f"POST XSCL {xscale} YSCL {yscale}\n")
-
+            cellSize = (2*xscale - 1, 2*yscale - 1)
 
 curses.wrapper(drawBoard)
